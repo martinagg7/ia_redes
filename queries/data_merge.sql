90652c092f0d14960cb52784940dd3406e5ae11b
Martina Garcia <martinagg@myuax.com>
2025-05-08 14:47:13 +0200

    up: eda and csv for rna

--- queries/data_merge.sql
+++ queries/data_merge.sql
@@ -1,15 +1,38 @@
-/*Para integrar la información de todas las tablas disponibles hacemos  LEFT JOIN
- tomando como tabla base MONGO06_Sociodemograficos, que contiene los datos sociodemográficos de todos los pacientes*/
- 
-SELECT *
+SELECT
+    -- Clave única del paciente
+    soc.paciente_id,
+
+    -- Sociodemográficos
+    soc.edad, soc.sexo, soc.estado_civil, soc.nivel_educativo, soc.ocupacion,
+    soc.region, soc.pais_nacimiento, soc.codigo_postal,
+
+    -- Generales
+    genl.fumador, genl.alcohol, genl.actividad_fisica, genl.vive,
+
+    -- Clínicos
+    cli.diabetes, cli.hipertension, cli.obesidad, cli.cancer,
+    cli.enfermedad_cardiaca, cli.asma, cli.epoc,
+
+    -- Bioquímicos
+    bio.glucosa, bio.colesterol, bio.trigliceridos,
+    bio.hemoglobina, bio.leucocitos, bio.plaquetas, bio.creatinina,
+
+    -- Genéticos
+    gen.mut_BRCA1, gen.mut_TP53, gen.mut_EGFR,
+    gen.mut_KRAS, gen.mut_PIK3CA, gen.mut_ALK, gen.mut_BRAF,
+
+    -- Económicos
+    eco.ingresos_mensuales, eco.gastos_salud, eco.seguro_salud,
+    eco.deudas, eco.tipo_empleo, eco.ayudas_publicas
+
 FROM [DATAEX].[MONGO06_Sociodemograficos] soc
-LEFT JOIN [DATAEX].[MONGO05_Generales] gen
-    ON soc.paciente_id = gen.paciente_id
-LEFT JOIN [DATAEX].[MONGO02_Clinicos] cli
+LEFT JOIN [DATAEX].[MONGO05_Generales] genl
+    ON soc.paciente_id = genl.paciente_id
+LEFT JOIN  [DATAEX].[MONGO02_Clinicos]cli
     ON soc.paciente_id = cli.paciente_id
 LEFT JOIN [DATAEX].[MONGO01_Bioquimicos] bio
     ON soc.paciente_id = bio.paciente_id
-LEFT JOIN [DATAEX].[MONGO03_Geneticos] genm
-    ON soc.paciente_id = genm.paciente_id
+LEFT JOIN [DATAEX].[MONGO03_Geneticos] gen
+    ON soc.paciente_id = gen.paciente_id
 LEFT JOIN [DATAEX].[MONGO04_Economicos] eco
     ON soc.paciente_id = eco.paciente_id
